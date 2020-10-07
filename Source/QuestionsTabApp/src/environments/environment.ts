0f9ea78675eefcc00fc5a926905cece49ac7f8c1
MyaThazinAung2020 <60773535+MyaThazinAung2020@users.noreply.github.com>
2020-10-07 14:02:42 +0630

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemomtza.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemomtza-questions.azurewebsites.net",
+    apiBaseUrl: "https://mtzaqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://mtzaqbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "aa81ea33-cf73-444b-b305-006b188a8fb4",
-        clientId: "2c35d24f-b159-4fc4-b9cd-ec85ff557330",
+        clientId: "83ab00f9-0dd5-4da0-a57b-5deaaf6ffba8",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
