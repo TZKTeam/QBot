39dbaed5ef940669d42cbf3b5eac32d448553b9d
MyaThazinAung2020 <60773535+MyaThazinAung2020@users.noreply.github.com>
2020-10-07 11:38:53 +0630

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotzchay.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotdemomtza.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
