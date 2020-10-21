9c3255270ea3a8b4d05d35707d832ea5e11a2c2e
MyaThazinAung2020 <60773535+MyaThazinAung2020@users.noreply.github.com>
2020-10-21 11:25:14 +0630

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://mthazinademoqbot.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://okissqbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "cab2f6ad-c37f-48f6-bb53-3bb35a150091",
-        clientId: "386e13d8-0f7c-4226-a122-446901e4fa84",
+        clientId: "b27dd14b-2fd8-430e-8461-e2b857d76a7a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
