0526c60732ac4ffd653c34289ff0bafd362761bf
MyaThazinAung2020 <60773535+MyaThazinAung2020@users.noreply.github.com>
2020-10-21 11:30:28 +0630

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://mthazinademoqbot.azurewebsites.net/api/Request/",
-    selfUrl: "https://mthazinademoqbot-questions.azurewebsites.net",
+    apiBaseUrl: "https://okissqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://okissqbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "cab2f6ad-c37f-48f6-bb53-3bb35a150091",
-        clientId: "386e13d8-0f7c-4226-a122-446901e4fa84",
+        clientId: "b27dd14b-2fd8-430e-8461-e2b857d76a7a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
